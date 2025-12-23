a71fa6e9151113a3bcf9e99150f6070e93e1f339
nshutipendo <nshutiemmanuelkwihangana@gamil.com>
2025-12-23 08:19:29 -0800

    My first commit

--- public/index.php
+++ public/index.php
@@ -7,8 +7,9 @@ require __DIR__ . '/../pages/welcometop.php';
 <div id = 'app'>
 <?php require __DIR__ . '/views/sign-in.php';
 ?>
+
 </div>
 
 <?php
 require __DIR__ . '/../pages/footer.php';
-?>
+?>z
